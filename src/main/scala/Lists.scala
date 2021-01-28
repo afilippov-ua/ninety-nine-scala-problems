94969637ab6155cb372ee8b155cc739f0be8cf3d
Oleksandr Filippov <afilippov.ua@gmail.com>
2021-01-29 00:01:31 +0200

    penultimate function was implemented

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -1,4 +1,6 @@
 object Lists {
 
   def last[A](as: List[A]): A = as.last
+
+  def penultimate[A](as: List[A]): A = as.takeRight(2).head
 }
--- src/test/scala/ListsSuite.scala
+++ src/test/scala/ListsSuite.scala
@@ -1,14 +1,22 @@
-import Lists.last
+import Lists.{last, penultimate}
 import org.scalatest.funsuite.AnyFunSuite
 
 class ListsSuite extends AnyFunSuite {
 
-  test("Getting the last element of a list has to return 8") {
+  test("P01 - Getting the last element of a list has to return 8") {
     assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
   }
 
-  test("Getting the last element of an empty list has to throw an exception") {
+  test("P01 - Getting the last element of an empty list has to throw an exception") {
     assertThrows[NoSuchElementException](last(List()))
+  }
+
+  test("P02 - Getting the penultimate element of a list has to return 8") {
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+  }
+
+  test("P02 - Getting the penultimate element of an empty list has to throw an exception") {
+    assertThrows[NoSuchElementException](penultimate(List()))
   }
 
 }
