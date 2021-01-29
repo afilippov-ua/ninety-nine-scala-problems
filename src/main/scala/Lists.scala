0fa596b9bee4ee7be98fabbb89f15855d5831b4e
Oleksandr Filippov <afilippov.ua@gmail.com>
2021-01-29 16:59:41 +0200

    replace high level collection functions with direct algo implementation

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -2,5 +2,36 @@ object Lists {
 
   def last[A](as: List[A]): A = as.last
 
-  def penultimate[A](as: List[A]): A = as.takeRight(2).head
+  def penultimate[A](as: List[A]): A = {
+    @annotation.tailrec
+    def go(cur: List[A], prev: List[A]): A = {
+      if (cur == Nil) {
+        prev.head
+      } else {
+        go(cur.tail, prev.tail)
+      }
+    }
+
+    go(skip(as, 2), as);
+  }
+
+  @annotation.tailrec
+  def skip[A](as: List[A], n: Int): List[A] = {
+    if (n == 0 || as.isEmpty)
+      as
+    else
+      skip(as.tail, n - 1);
+  }
+
+  def nth[A](n: Int, as: List[A]): A = skip(as, n).head
+
+  def length[A](as: List[A]): Int = {
+    @annotation.tailrec
+    def go[A](s: List[A], n: Int): Int = s match {
+      case h :: t => go(t, n + 1)
+      case Nil => n
+    }
+
+    go(as, 0);
+  }
 }
--- src/test/scala/ListsSuite.scala
+++ src/test/scala/ListsSuite.scala
@@ -1,4 +1,4 @@
-import Lists.{last, penultimate}
+import Lists.{last, length, penultimate}
 import org.scalatest.funsuite.AnyFunSuite
 
 class ListsSuite extends AnyFunSuite {
@@ -11,12 +11,24 @@ class ListsSuite extends AnyFunSuite {
     assertThrows[NoSuchElementException](last(List()))
   }
 
-  test("P02 - Getting the penultimate element of a list has to return 8") {
+  test("P02 - Getting the penultimate element of a list has to return 5") {
     assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
   }
 
   test("P02 - Getting the penultimate element of an empty list has to throw an exception") {
     assertThrows[NoSuchElementException](penultimate(List()))
+  }
+
+  test("P04 - length of empty list has to return 0") {
+    assert(length(List()) == 0)
+  }
+
+  test("P04 - length of a list with 1 element has to return 1") {
+    assert(length(List(1)) == 1)
+  }
+
+  test("P04 - length of list with 6 elements has to return 6") {
+    assert(length(List(1, 1, 2, 3, 5, 8)) == 6)
   }
 
 }
